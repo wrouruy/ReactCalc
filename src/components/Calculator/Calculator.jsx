2411ecea8c99dd8b5337c347e3a0236729d35354
wrouruy <wroyruy@gmail.com>
2025-07-30 17:13:32 +0300

    second commit

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -2,44 +2,58 @@ import React, { useState, useRef } from 'react';
 import ButtonItem from '../ButtonItem/ButtonItem';
 import './Calculator.css'
 
-const symbols = ['AC', '+/-', '%', '/', 7, 8, 9, '*', 4, 5, 6, '-', 1, 2, 3, '+', '#', 0, '.', '='];
+const symbols = ['AC', '+/-', '%', '÷', 7, 8, 9, '×', 4, 5, 6, '-', 1, 2, 3, '+', '#', 0, '.', '='];
 
 export default function Calculator() {
     const monitorRef       = useRef('');
     const historyContainer = useRef(null);
-    const globalResult     = useRef('');
+    const globalResult     = useRef('0');
     const [history, setHistory] = useState([]);
 
-    const operationSum = [ '+', '-', '/', '*', '.', '%' ]
+    const operationSum = [ '+', '-', '÷', '×', '%' ]
     const operationChange = {
         '=': () => {
+            if(operationSum.includes(globalResult.current.at(-1)) || globalResult.current.at(-1) == '.') return;
             setHistory([...history, globalResult.current]);
 
-            const solved = eval(globalResult.current);
-            monitorRef.current.innerText = globalResult.current = (String(solved).length > 3 ? solved.toFixed(3) : solved);
+            const solved = eval(changeSymbols(globalResult.current));
+            monitorRef.current.innerText = globalResult.current = String(String(solved).includes('.') ? (String(solved).split('.').at(-1).length > 3 ? solved.toFixed(3) : solved) : String(solved));
             historyContainer.current.scrollTop += historyContainer.current.scrollHeight;
-
         },
-        '+/-': () => globalResult.current = eval(globalResult.current) * -1,
-        'AC': () => globalResult.current = '',
-        '#': () => window.open('https://github.com/wrouruy') // я задоволений
+        '+/-': () => globalResult.current = String(eval(changeSymbols(globalResult.current)) * -1),
+        'AC':  () => globalResult.current = '0',
+        '#':   () => window.open('https://github.com/wrouruy') // я задоволений
     }
 
     // fill in the corresponding buttons with color
     const colorToSymbol = {};
     [7, 8, 9, 4, 5, 6, 1, 2, 3, '#', 0, '.'].forEach(el => colorToSymbol[el] = '#5E6960');
     ['AC', '+/-', '%'].forEach(el => colorToSymbol[el] = '#7D877F');
-    ['/', '*', '-', '+', '='].forEach(el => colorToSymbol[el] = '#FF951B');
+    ['÷', '×', '-', '+', '='].forEach(el => colorToSymbol[el] = '#FF951B');
+
+    function changeSymbols(symbols){
+        return symbols.replace(/[×]/g, '*')
+                      .replace(/[÷]/g, '/');
+    }
 
     function sumBtn(symbol){
-        if(typeof symbol == 'number' || operationSum.includes(symbol))
+        const splitExample = globalResult.current.split(new RegExp(operationSum.map((str) => str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')).join('|'), 'g'));
+
+        if(typeof symbol == 'number' && globalResult.current !== '0')
             globalResult.current += String(symbol);
+
+        if(typeof symbol == 'number' && globalResult.current === '0')
+            globalResult.current = String(symbol);
+
+        if(operationSum.includes(symbol) && !operationSum.includes(globalResult.current.at(-1)) ||
+            symbol == '.' && !operationSum.includes(globalResult.current.at(-1)) && !splitExample.at(-1).includes('.')){
+            globalResult.current += symbol;
+        }
 
         if(Object.keys(operationChange).includes(symbol))
             operationChange[symbol]();
 
         monitorRef.current.innerText = globalResult.current;
-        // monitorRef.current.scrollLeft =  - monitorRef.current.clientWidth;
         monitorRef.current.scrollTo({
             left: monitorRef.current.scrollWidth - monitorRef.current.clientWidth,
             behavior: 'smooth'
@@ -58,7 +72,6 @@ export default function Calculator() {
                 <div key={el} onClick={() => sumBtn(el)}>
                     <ButtonItem symbol={el} bgcolor={colorToSymbol[el]}/>
                 </div>
-            
             ))}
         </div>
     )
